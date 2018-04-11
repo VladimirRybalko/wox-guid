2dcf7071208d8ba42a1a4ceb98abfd537f7616e9
Vladimir Rybalko <vladimir.a.rybalko@gmail.com>
2018-04-11 14:34:45 +0200

    Initial commit

---
+++ src/GuidGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Wox.Plugin.Guid
+{
+    public sealed class GuidGenerator : IPlugin
+    {
+        public void Init(PluginInitContext context)
+        {
+        }
+
+        public List<Result> Query(Query query)
+        {
+            var guid = System.Guid.NewGuid().ToString();
+
+            return new List<Result>
+            {
+                new Result
+                {
+                    Title = guid,
+                    Action = e =>
+                    {
+                        Clipboard.SetText(guid);
+                        return true;
+                    }
+                },
+
+                new Result
+                {
+                    Title = guid.ToUpper(),
+                    Action = e =>
+                    {
+                        Clipboard.SetText(guid);
+                        return true;
+                    }
+                }
+            };
+        }
+    }
+}
